60a6c2c21aebc50d7e1228dd5ca8ed163d67c581
Diatoz <diatoz@Diatozs-MacBook-Pro.local>
2023-12-04 13:26:48 +0530

    sound implemented

---
+++ App.js
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import SoundImplementation from './src/components/SoundImplementation';
+
+const App = () => {
+  return (
+    <View>
+      <SoundImplementation />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/components/SoundImplementation.js
@@ -0,0 +1,49 @@
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Sound from 'react-native-sound';
+
+const SoundImplementation = () => {
+  const playsound = () => {
+    var sound = new Sound('whoosh-transition.mp3', Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // loaded successfully
+      console.log(
+        'duration in seconds: ' +
+          sound.getDuration() +
+          'number of channels: ' +
+          sound.getNumberOfChannels(),
+      );
+
+      // Play the sound with an onEnd callback
+      sound.play(success => {
+        if (success) {
+          console.log('successfully finished playing');
+        } else {
+          console.log('playback failed due to audio decoding errors');
+        }
+      });
+    });
+  };
+  return (
+    <View>
+      <Text
+        onPress={() => playsound()}
+        style={{
+          borderWidth: 1,
+          backgroundColor: 'pink',
+          padding: 20,
+          alignSelf: 'center',
+          margin: 20,
+        }}>
+        play sound
+      </Text>
+    </View>
+  );
+};
+
+export default SoundImplementation;
+
+const styles = StyleSheet.create({});
